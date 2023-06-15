b117ab92f68438b5e92b3adbfdad108db972ec91
rqthomas <rqthomas@vt.edu>
2023-06-15 11:32:43 -0400

    adding first function

---
+++ R/fit_hetgp.R
@@ -0,0 +1,17 @@
+#' Title
+#'
+#' @param df
+#' @param reference_datetime
+#' @param horizon
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+fit_hetgp <- function(df, reference_datetime, horizon){
+  #add stuff
+
+  return(df)
+
+}

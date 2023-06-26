d625990cd4ec7e547756111483c427235c1ec3b2
maikeh7 <mholthuijzen@alumni.uidaho.edu>
2023-06-26 13:49:00 -0400

    added stuff to fit_hetgp.R

--- R/fit_hetgp.R
+++ R/fit_hetgp.R
@@ -1,17 +1,26 @@
 #' Title
 #'
-#' @param df
-#' @param reference_datetime
-#' @param horizon
+#' @param X 
+#' @param Z
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #'
-fit_hetgp <- function(df, reference_datetime, horizon){
-  #add stuff
+fit_hetgp = function(X, Z){
+
+  het_gp_fit = mleHetGP(X, Z, covtype =  "Gaussian")
+  het_gp_fit = rebuild(het_gp_fit, robust = TRUE)
 
   return(df)
 
 }
+
+predict_hetgp = function(Xnew, het_gp_fit){
+  Xnew = as.matrix(Xnew)
+  preds = predict(Xnew)
+  return(preds)
+}
+
+
